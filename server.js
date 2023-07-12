bd12e5921707731e81e041349307149228e8e754
Jordan Ball <cjordanball@ballwebdev.com>
2023-07-12 19:38:41 -0400

    add body-parser to server

--- server.js
+++ server.js
@@ -1,16 +1,28 @@
 import http from 'http';
 import express from 'express';
+import bodyParser from 'body-parser';
 
 // Create express app
 const app = express();
 
-// we want to put longer routes first, because express will check from top to bottom
-// and if we put '/' first, it will match all the routes
-app.use('/dogs', (req, res, next) => {
+// this middleware will trigger for all incoming requests
+// it will parse the body of the request and add it to the request object
+// it will also automatically call the next() function
+// bodyParser only works for POST requests
+// extended: true allows us to parse extended bodies with rich data in it
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use('/add-product', (req, res, next) => {
 	console.log('In the second middleware!');
 	res.send(`
-		<h1>Woof! from Express!</h1>
+		<form action="\product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>
 	`);
+});
+
+app.post('/product', (req, res, next) => {
+	// this middleware will only trigger for POST request
+	console.log(req.body);
+	res.redirect('/');
 });
 
 app.use('/', (req, res, next) => {
